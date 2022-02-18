827caa7a954b2d88d0729ad7746259badef019d0
chenyy <yangyichen@trip.com>
2022-02-18 18:02:06 +0800

    test

---
+++ config/dependenciesV2.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+
+    implementation(Libs.KOTLIN_STDLIB)
+    implementation(Libs.JUNIT)
+    implementation(Libs.EXT_JUNIT)
+    implementation(Libs.ESPRESSO_CORE)
+    implementation(Libs.CORE_KTX)
+    implementation(Libs.APPCOMPAT)
+    implementation(Libs.MATERIAL)
+}
---
+++ config/module_build_configV2.gradle.kts
@@ -0,0 +1,33 @@
+android {
+    compileSdk = Versions.COMPILE_SDK
+//    buildToolsVersion project.properties.'buildToolsVersion'
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+        versionCode = Versions.versionCodeMobile
+        versionName = Versions.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles = "consumer-rules.pro"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+        options.jvmTarget = "1.8"
+    }
+}
