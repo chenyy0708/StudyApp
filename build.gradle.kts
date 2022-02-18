90c3ae77bcc9f98a3f3610d21652647eb933296f
chenyy <yangyichen@trip.com>
2022-02-18 18:36:47 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 buildscript {
 
     repositories {
         google()
         mavenCentral()
-        jcenter()
-        // Android Build Server
-        maven {
-            url = uri("${project.rootDir}/Plugin/plugins")
-        }
+        maven(url = "${project.rootDir}/Plugin/plugins")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}")
@@ -28,36 +22,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven(url = "https://maven.aliyun.com/repository/public")
     }
 }
 
-subprojects {
-    apply(plugin = "com.diffplug.gradle.spotless")
-    val ktlintVer = "0.40.0"
-    spotless {
-        kotlin {
-            target("**/*.kt")
-            ktlint(ktlintVer).userData(
-                mapOf("max_line_length" to "100", "disabled_rules" to "import-ordering")
-            )
-            licenseHeaderFile(project.rootProject.file("copyright.kt"))
-        }
-        kotlinGradle {
-            // same as kotlin, but for .gradle.kts files (defaults to '*.gradle.kts')
-            target("**/*.gradle.kts")
-            ktlint(ktlintVer)
-            licenseHeaderFile(project.rootProject.file("copyright.kt"), "(plugins |import |include)")
-        }
-    }
-
-    tasks.whenTaskAdded {
-        if (name == "preBuild") {
-            mustRunAfter("spotlessCheck")
-        }
-    }
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions.freeCompilerArgs +=
-            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
-    }
-}
